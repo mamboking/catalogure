2cfb1400a15cad02a9e7ebe1869d56be6b716c9e
Kevin Hostelley <khostelley@yahoo.com>
2011-07-20 09:18:36 -0400

    Initial

---
+++ src/catalogure/core.clj
@@ -0,0 +1,5 @@
+(ns catalogure.core)
+
+(defrecord Catalog [id name description version])
+
+(defrecord Item [id name description version])
---
+++ test/catalogure/test/core.clj
@@ -0,0 +1,6 @@
+(ns catalogure.test.core
+  (:use [catalogure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
