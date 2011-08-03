1a08beaa41bea141774854fe2df93e540cb0853b
Kevin Hostelley <khostelley@yahoo.com>
2011-08-03 14:49:06 -0400

    Added a list of stories to work on.

--- src/catalogure/core.clj
+++ src/catalogure/core.clj
@@ -2,4 +2,11 @@
 
 (defrecord Catalog [id name description version])
 
-(defrecord Item [id name description version])
+(defrecord TagHierarchy [id name description tag-tree])
+
+(defrecord Item [id name description version attributes tags parent-item])
+
+(defrecord ItemGroup [id name description version parent-item items])
+
+(defrecord CatalogElements [id catalogId elements])
+
